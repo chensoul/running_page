56ea527112b609920e769b6b3b75345b3307287a
chensoul <ichensoul@gmail.com>
2024-09-03 11:52:13 +0800

    in

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -13,11 +13,15 @@ const data: ISiteMetadataResult = {
   siteTitle: 'ChenSoul Exercise',
   siteUrl: 'https://run.chensoul.cc',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  description: 'ChenSoul Exercise Page',
   navLinks: [
     {
       name: 'Blog',
       url: 'https://blog.chensoul.cc',
+    },
+    {
+      name: 'Github',
+      url: 'https://github.com/chensoul',
     }
   ],
 };
