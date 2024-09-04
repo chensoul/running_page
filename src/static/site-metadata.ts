ff119b4372352968270d2f6e32d42346d6b2b1fd
chensoul <ichensoul@gmail.com>
2024-09-04 15:22:04 +0800

    update log

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -12,7 +12,7 @@ interface ISiteMetadataResult {
 const data: ISiteMetadataResult = {
   siteTitle: 'ChenSoul 运动',
   siteUrl: 'https://run.chensoul.cc',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  logo: 'https://avatars.githubusercontent.com/u/826932',
   description: 'ChenSoul 运动：跑步、骑行、健走',
   navLinks: [
     {
